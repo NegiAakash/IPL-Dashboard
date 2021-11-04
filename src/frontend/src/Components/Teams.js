416280866f4d112878ce9a0c226e7013006eced1
Aakash Negi <aakash.negi011@gmail.com>
2021-11-04 21:03:38 +0530

    Completed build process of both frontend and backend

--- src/frontend/src/Components/Teams.js
+++ src/frontend/src/Components/Teams.js
@@ -6,7 +6,7 @@ export default function Teams() {
   const [teams, setTeams] = useState([]);
   useEffect(() => {
     const fetchAllTeams = async () => {
-      const response = await fetch(`http://localhost:8080/all`);
+      const response = await fetch(`/all`);
       const data = await response.json();
       setTeams(data);
     };
@@ -18,10 +18,8 @@ export default function Teams() {
       <h2 className="team-text">IPL Dashboard</h2>
       <div className="HomePage">
         {teams.map((team) => (
-          <Link to={`/team/${team.teamName}`}>
-            <div className="team " key={team.id}>
-              {team.teamName}
-            </div>
+          <Link to={`/team/${team.teamName}`} key={team.id}>
+            <div className="team ">{team.teamName}</div>
           </Link>
         ))}
       </div>
